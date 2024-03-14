eb2edf5acad419f9a966e3c157ab7e35ea62ddd5
jeongho.park1993 <jeongho.park1993@gmail.com>
2024-03-14 18:18:46 +0900

    test

--- tp01/src/main/java/kr/spring/board/exception/BoardErrorCode.java
+++ tp01/src/main/java/kr/spring/board/exception/BoardErrorCode.java
@@ -3,5 +3,5 @@ package kr.spring.board.exception;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
-public enum BoardErrorCode {	
+public enum BoardErrorCode {
 }
