16cdf5d413cc492486b76580db6c0d637b2e861b
jeongho.park1993 <jeongho.park1993@gmail.com>
2024-03-31 19:17:57 +0900

    commit & push

--- tp01/src/main/java/kr/spring/board/exception/BoardException.java
+++ tp01/src/main/java/kr/spring/board/exception/BoardException.java
@@ -6,12 +6,15 @@ import org.springframework.http.HttpStatus;
 
 public class BoardException extends RuntimeException {
 	
+	//ExceptionCode 넣기
 	private final BoardExceptionCode boardExceptionCode;
 	
+	//ExceptionCode를 생성자로부터 받아서 세팅
 	public BoardException(BoardExceptionCode boardExceptionCode) {
 		this.boardExceptionCode = boardExceptionCode;
 	}
 	
+	//getters
 	public BoardExceptionCode getBoardExceptionCode() {
 		return boardExceptionCode;
 	}
